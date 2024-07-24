d287bbc19fe35ab1e8fc8196439bbdcbe1b9a529
Riadiwibowo <riadiwibowo777@gmail.com>
2024-07-25 00:41:09 +0700

    modified some files

--- src/features/auth/index.tsx
+++ src/features/auth/index.tsx
@@ -10,28 +10,28 @@ const Authentication = () => {
   const handleSubmit = async (e: FormEvent) => {
     e.preventDefault();
     console.log(e);
-
     const payload = { username, password };
-
     try {
       const response = await postLogin(payload);
-      localStorage.setItem("token", response?.token as string);
-      navigate("/");
+      if (response?.token) {
+        localStorage.setItem("token", response?.token as string);
+        navigate("/");
+      }
     } catch (error) {
       console.log(error);
     }
-  };
-
+  }
   return (
     <div>
       <form onSubmit={handleSubmit} className="flex flex-col justify-center items-center h-screen gap-2">
         <label> Authentication</label>
-        <input type="text" onChange={(e: ChangeEvent<HTMLInputElement>) => setUsername(e.target.value)} className="border border-gray-400 rounded-sm"></input>
-        <input type="password" onChange={(e: ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)} className="border border-gray-400 rounded-none"></input>
+        <input type="text" onChange={(e: ChangeEvent<HTMLInputElement>) => setUsername(e.target.value)} 
+        className="border border-gray-400 rounded-sm"></input>
+        <input type="password" onChange={(e: ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)} 
+        className="border border-gray-400 rounded-none"></input>
         <button className="bg-black text-white rounded-sm py-1 px-5">Login</button>
       </form>
     </div>
   );
 };
-
 export default Authentication;
